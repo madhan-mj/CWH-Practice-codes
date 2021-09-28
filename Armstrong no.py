6ea2170126cb66ea4ba09c6dc92f8d060a0a26cd
Madhan-mj <89994853+madhan-mj@users.noreply.github.com>
2021-09-28 15:01:09 +0530

    Update Armstrong no.py

--- Armstrong no.py
+++ Armstrong no.py
@@ -18,3 +18,19 @@ for number in range(num1,num2+1):
 
     if sum == number:
         print(sum, end=", ")
+        
+        
+        #OR Using While loop
+        
+n = int(input("Enter a number : "))
+sum = 0
+order = len(str(n))
+copy_n = n
+while(n>0):
+    digit = n % 10
+    sum+=digit**order
+    n= n//10
+if(sum==copy_n):
+    print("Armstrong number")
+else:
+    print("its not a armstrong number")
